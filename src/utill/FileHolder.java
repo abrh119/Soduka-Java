f3bf82b486af72fae0c37d26f63c47bb4e6ce2ac
abrh119 <59731843+abrh119@users.noreply.github.com>
2021-06-26 21:06:19 +0530

    Add files via upload

---
+++ src/utill/FileHolder.java
@@ -0,0 +1,20 @@
+package utill;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ *
+ */
+public class FileHolder {
+
+    public static String LEVEL1 = "src/levels/level1.txt";
+    public static String LEVEL2 = "src/levels/level2.txt";
+    public static String LEVEL3 = "src/levels/level3.txt";
+    public static String LEVEL4 = "src/levels/level4.txt";
+    public static String LEVEL5 = "src/levels/level5.txt";
+
+}
---
+++ src/utill/FixedMapElements.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utill;
+
+/**
+ *
+ *
+ */
+public class FixedMapElements {
+
+    /**
+     * The string representation of outside of wall
+     */
+    public static final char OUTSIDE_OF_WALL = '+';
+    public static final String OUTSIDE_OF_WALL_STRING = "+";
+
+    /**
+     * The string representation of path
+     */
+    public static final char FLOOR = ' ';
+    public static final String FLOOR_STRING = " ";
+
+    /**
+     * The string representation of wall
+     */
+    public static final char WALL = '#';
+    public static final String WALL_STRING = "#";
+
+    /**
+     * The string representation of crate
+     */
+    public static final char CRATE = '$';
+    public static final String CRATE_STRING = "$";
+
+    /**
+     * The string representation of warehouse keeper
+     */
+    public static final char WAREHOUSE_KEEPER = '@';
+    public static final String WAREHOUSE_KEEPER_STRING = "@";
+
+    /**
+     * The string representation of diamond
+     */
+    public static final char DIAMOND = '.';
+    public static final String DIAMOND_STRING = ".";
+
+}
